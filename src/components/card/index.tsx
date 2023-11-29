4fed285359ad2326067eebc78e5f21a03b24fa82
luis.couto <luis.couto@frgnet.com.br>
2023-11-29 08:07:20 -0300

    fix(projetos): modificado para exibir as imgs certas

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
-import { ProjectCardProps } from "./types";
 import Image from "next/image";
+import { ProjectCardProps } from "./types";
 
 const ProjectCard: FC<ProjectCardProps> = (props) => {
   const { description, imageUrl, onClick } = props;
@@ -10,7 +10,7 @@ const ProjectCard: FC<ProjectCardProps> = (props) => {
       className="flex flex-col items-center justify-center project-card border-2 cursor-pointer h-50 w-80"
       onClick={onClick}
     >
-      <Image className="mb-2 h-28 w-70" src={imageUrl} alt="Project Image" />
+      <Image className="mb-2 h-auto w-70" src={imageUrl} alt={description} />
       <p>{description}</p>
     </div>
   );
