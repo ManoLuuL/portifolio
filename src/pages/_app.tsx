9820731cdec17f064f6e909b409c87c32fe72277
luis.couto <luis.couto@frgnet.com.br>
2023-12-07 11:02:58 -0300

    fix(app): modificado estrutura e adicionado novo titulo e favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,15 @@ import "tailwindcss/tailwind.css";
 
 import type { AppProps } from "next/app";
 import { AppProvider } from "@/globals/context/app-context";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppProvider>
+      <Head>
+        <title>Portifólio - Luis Ricardo</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </AppProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,18 +5,38 @@ import { skillTeste } from "@/globals/consts";
 export default function Home() {
   return (
     <Layout>
-      <section className="pb-5 flex justify-center items-center">Sobre</section>
-
-      <section className="pb-5 flex justify-center">
+      <section className="pb-5 flex justify-center mt-10 mb-20">
         <div className="flex flex-col items-center w-[90%]">
-          <h2>Habilidades</h2>
+          <h2 className="font-semibold mb-14 text-gray-600 dark:text-gray-200 text-3xl">
+            Sobre
+          </h2>
+        </div>
+      </section>
+
+      <section className="pb-5 flex justify-center mt-10 mb-20">
+        <div className="flex flex-col items-center w-[90%]">
+          <h2 className="font-semibold mb-14 text-gray-600 dark:text-gray-200 text-3xl">
+            Habilidades
+          </h2>
           <div className="flex items-center gap-20 flex-wrap">
             <SkillCard skils={skillTeste} />
           </div>
         </div>
       </section>
-      <section className="pb-5">Projetos</section>
-      <section className="pb-5">Contatos</section>
+      <section className="pb-5 flex justify-center mt-10 mb-20">
+        <div className="flex flex-col items-center w-[90%]">
+          <h2 className="font-semibold mb-14 text-gray-600 dark:text-gray-200 text-3xl">
+            Projetos
+          </h2>
+        </div>
+      </section>
+      <section className="pb-5 flex justify-center mt-10 mb-20">
+        <div className="flex flex-col items-center w-[90%]">
+          <h2 className="font-semibold mb-14 text-gray-600 dark:text-gray-200 text-3xl">
+            Contato
+          </h2>
+        </div>
+      </section>
     </Layout>
   );
 }
