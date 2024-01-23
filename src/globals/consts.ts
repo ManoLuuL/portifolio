bc2cdcf04486047462c78c81a7ee7a7b2966a6d0
luis.couto <luis.couto@frgnet.com.br>
2024-01-23 17:44:43 -0300

    fix(globals): modificando projetos

--- src/globals/consts.ts
+++ src/globals/consts.ts
@@ -88,11 +88,13 @@ export const Projects: ProjectProps[] = [
     src: MiniRPG,
     icons: [IconTypeScript(30), IconReact(30)],
     situation: "Desenvolvimento",
+    link: "https://github.com/ManoLuuL/MiniRpg2D",
   },
   {
     title: "Sistema de Tarefas",
     src: Task,
     icons: [IconJavaScript(30), IconReact(30)],
+    link: "https://github.com/ManoLuuL/SistemaDeTask",
     situation: "Concluido - 17/04/22",
   },
   {
@@ -100,5 +102,6 @@ export const Projects: ProjectProps[] = [
     src: TemplateADM,
     icons: [IconTypeScript(30), IconReact(30), IconNextJS(30)],
     situation: "Concluido - 02/11/23",
+    link: "https://github.com/ManoLuuL/template-admin",
   },
 ];
